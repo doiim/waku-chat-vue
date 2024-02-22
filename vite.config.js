b18bdc8ae2551699304344834cab9e19dc7b7ee3
Filipe Soccol <filipe.soccol@gmail.com>
2024-02-22 10:50:33 -0300

    Updated Vite, vite-plugin and reset package.lock.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: '/waku-chat-vue/'
+    base: '/waku-chat-vue/',
+    resolve: {
+        preserveSymlinks: true,
+    },
 })
