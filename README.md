127b7951b9f4a62881c5964fb2fd7d87430f432c
raprogue <guilherme@doiim.com>
2024-03-25 14:41:58 -0300

    transform project to use plugin

--- README.md
+++ README.md
@@ -1,22 +1,9 @@
-# LibP2P + PubSub + Vue + Typescript
 
-A minimal demonstration of how to use `libp2p + pub-sub` with `Vue`. You can send messages to a public chat and also direct to specific peer id.
+## Installation and running
 
-[Live Demo!](https://doiim.github.io/waku-chat-vue/)
-
-![screenshot of the js ipfs node id info](https://github.com/filipesoccol/libp2p-pubsub-vue/assets/13040410/0afce13c-1910-40c3-93ea-05237905be18)
-
-This project was bootstrapped with [Vue CLI](https://cli.vuejs.org/).
-
-## Installation and running Client
-
-First clone this repo, install dependencies in the project root and build the project.
+Install dependencies in the project and run the project.
 
 ```sh
-$ git clone https://github.com/doiim/waku-chat-vue.git
-$ cd waku-chat-vue
 $ npm install
 $ npm run dev
 ```
-
-## Consider running your own node for Waku network
--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,91 @@
-<script setup lang="ts">
-import WakuChat from "./components/WakuChat.vue";
-</script>
-
 <template>
   <div id="app">
-    <img id="libp2p-logo" alt="IPFS logo" src="./assets/waku-logo.svg" />
-    <WakuChat />
+    <h1>Vue Waku Tester</h1>
+    <p>This is a simple page test with the ability to change your name and id.</p>
+
+    <div class="input-container">
+      <label for="idInput">ID:</label>
+      <input type="text" v-model="idInput" id="idInput">
+      <button @click="changeId">Change ID</button>
+    </div>
+
+    <div class="input-container">
+      <label for="nameInput">Name:</label>
+      <input type="text" v-model="nameInput" id="nameInput" placeholder="Type your name">
+      <button @click="changeName">Change Name</button>
+    </div>
+
+    <ChatComponent :externalUserId="externalId" />
   </div>
 </template>
 
-<style>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const idInput = ref('thisIsMyIdDefinedByMyApplication');
+const nameInput = ref('');
+
+const externalId = ref('thisIsMyIdDefinedByMyApplication');
+
+const changeId = () => {
+  externalId.value = idInput.value
+};
+
+const changeName = () => {
+  document.dispatchEvent(new CustomEvent('changeNickName', { detail: nameInput.value }));
+};
+</script>
+
+<style scoped>
 body {
   margin: 0;
+  font-family: Arial, sans-serif;
+  background-color: #f2f2f2;
 }
+
 #app {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
 }
-#libp2p-logo {
-  height: 10rem;
-  margin: 24px;
+
+h1 {
+  color: #333;
 }
-img {
-  background-color: black;
-  border-radius: 100%;
+
+p {
+  color: #666;
+}
+
+.input-container {
+  margin-bottom: 20px;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+  color: #333;
+}
+
+input[type="text"] {
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 15px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+button {
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #0056b3;
 }
 </style>
