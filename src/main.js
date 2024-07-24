6776379661ad4634abbf4b84efab298d07093cda
raprogue <guilherme@doiim.com>
2024-07-24 15:28:08 -0300

    update version to 0.8.17

--- src/main.js
+++ src/main.js
@@ -1,25 +1,27 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import WakuChatVuePlugin from '@doiim/waku-chat-vue-plugin';
+import { createApp } from "vue";
+import App from "./App.vue";
+import WakuChatVuePlugin from "@doiim/waku-chat-vue-plugin";
 
-createApp(App).use(WakuChatVuePlugin, {
-    wakuChannelName: 'my-app-name',
+createApp(App)
+  .use(WakuChatVuePlugin, {
+    wakuChannelName: "my-app-name",
     wakuPeers: [],
-    availableRooms: ['General', 'Support'],
-    changeNickMode: 'user',
+    availableRooms: ["General", "Support"],
+    changeNickMode: "user",
     allowPrivateChat: false,
     userChangeNick: true,
     showSettings: true,
     messageAgeToDownload: (4 * 24 + 7) * 60 * 60 * 1000,
-    disconnectDelay:5*60*1000,
-    styleConfig:{
-        colors:{
-            light:{
-                primary:'rgba(41, 79, 204, 1)',
-            },
-            dark:{
-                primary:'rgba(135, 79, 204, 1)',
-            }
-        }
-    }
-}).mount('#app');
+    disconnectDelay: 5 * 60 * 1000,
+    styleConfig: {
+      colors: {
+        light: {
+          primary: "rgba(41, 79, 204, 1)",
+        },
+        dark: {
+          primary: "rgba(135, 79, 204, 1)",
+        },
+      },
+    },
+  })
+  .mount("#app");
